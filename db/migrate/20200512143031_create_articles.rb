4a001ce5fffc2f79455459cdb80d4e02bdbb98b8
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2020-05-12 23:32:59 +0900

    generated filles by scaffold

---
+++ db/migrate/20200512143031_create_articles.rb
@@ -0,0 +1,15 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.stringrails :title
+      t.string :g
+      t.string :scaffold
+      t.string :article
+      t.string :title
+      t.string :author
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
